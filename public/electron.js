b20a4b533f5fa480db4bc505c5b0eab8ee8a1f5f
Stephen Hobdell <hobdell.stephen@gmail.com>
2023-08-05 12:23:47 +0100

    add preload script to electron app.

--- public/electron.js
+++ public/electron.js
@@ -2,22 +2,25 @@ const { app, BrowserWindow } = require('electron');
 const path = require('path');
 
 const createWindow = () => {
-    const win = new BrowserWindow({
-      width: 800,
-      height: 600
-    });
-  
-    win.loadFile(path.join(__dirname, '../build/index.html'));
-  };
-
-  app.whenReady().then(() => {
-    createWindow();
-  
-    app.on('activate', () => {
-      if (BrowserWindow.getAllWindows().length === 0) createWindow();
-    });
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js')
+    }
   });
 
-  app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin') app.quit();
+  win.loadFile(path.join(__dirname, '../build/index.html'));
+};
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
   });
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
